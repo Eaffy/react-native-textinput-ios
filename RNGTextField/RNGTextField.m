729bcb88b032158ce6959c5dc49c9aca122727f6
eaffy <yifei239@126.com>
2020-04-02 14:58:55 +0800

    fix issue#1

--- RNGTextField/RNGTextField.m
+++ RNGTextField/RNGTextField.m
@@ -1,40 +1,75 @@
 //
-//  RNGTextField.m
+//  RNGTextFieldManager.m
 //  taochemao
 //
 //  Created by Eafy on 2018/7/31.
 //  Copyright © 2018年 emao. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
+#import <React/RCTViewManager.h>
 #import "RNGTextField.h"
+#import <React/RCTFont.h>
 
-@implementation RNGTextField
+@interface RNGTextFieldManager : RCTViewManager<UITextFieldDelegate>
+@end
 
-/*
- // Only override drawRect: if you perform custom drawing.
- // An empty implementation adversely affects performance during animation.
- - (void)drawRect:(CGRect)rect {
- // Drawing code
- }
- */
+@implementation RNGTextFieldManager
 
-- (void)setPlaceholderTextColor:(UIColor *)placeholderTextColor
+RCT_EXPORT_MODULE()
+
+RCT_EXPORT_VIEW_PROPERTY(placeholder, NSString)
+RCT_EXPORT_VIEW_PROPERTY(maxLength, NSNumber)
+RCT_EXPORT_VIEW_PROPERTY(text, NSString)
+RCT_EXPORT_VIEW_PROPERTY(onChange, RCTBubblingEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onEndEditing, RCTBubblingEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(returnKeyType, UIReturnKeyType)
+RCT_CUSTOM_VIEW_PROPERTY(placeholderTextColor, UIColor, RNGTextField)
 {
-    _placeholderTextColor = placeholderTextColor;
-    [self _updatePlaceholder];
+    [view setValue: [RCTConvert UIColor:json] forKeyPath: @"_placeholderLabel.textColor"];
+}
+RCT_CUSTOM_VIEW_PROPERTY(textAlign, NSTextAlignment, RNGTextField)
+{
+    [view setTextAlignment:[RCTConvert NSTextAlignment:json]];
+}
+RCT_EXPORT_VIEW_PROPERTY(textColor, UIColor)
+RCT_CUSTOM_VIEW_PROPERTY(fontSize, NSNumber, RNGTextField)
+{
+    view.font = [RCTFont updateFont:view.font withSize:json ?: @(defaultView.font.pointSize)];
+}
+//RCT_CUSTOM_VIEW_PROPERTY(placeholderTextColor, UIColor, RNGTextField){
+//    view.placeholderTextColor = [RCTConvert UIColor:json];
+//}
+
+- (UIView *)view
+{
+    RNGTextField *textField = [[RNGTextField alloc] init];
+    textField.delegate = self;
+    [textField addTarget:self action:@selector(textFieldDidChanged:) forControlEvents:UIControlEventEditingChanged];
+    return textField;
 }
 
-- (void)_updatePlaceholder {
-    if (self.placeholder == nil) {
-        return;
+
+- (void)textFieldDidChanged:(RNGTextField *)textField {
+    UITextRange *range = textField.markedTextRange;
+    NSUInteger maxLength = textField.maxLength.unsignedIntegerValue;
+
+    if (!range && maxLength) {
+        NSString *str = textField.text;
+        if (str.length > maxLength) {
+            str = [str substringToIndex:maxLength];
+            textField.text = str;
+        }
     }
-    
-    NSMutableDictionary *attributes = [NSMutableDictionary new];
-    if (_placeholderTextColor) {
-        [attributes setObject:_placeholderTextColor forKey:NSForegroundColorAttributeName];
+    if (textField.onChange) {
+        textField.onChange(@{@"text": textField.attributedText.string});
     }
-    
-    self.attributedPlaceholder = [[NSAttributedString alloc] initWithString:self.placeholder attributes:attributes];
+}
+
+- (void)textFieldDidEndEditing:(RNGTextField *)textField {
+    if (textField.onEndEditing) {
+        textField.onEndEditing(@{@"text": textField.attributedText.string});
+    }
 }
 
 @end
